444be67168fa3667f2c9204836806d509954e498
idk <hankhill19580@gmail.com>
2021-02-09 12:22:55 -0500

    add a create command

--- commands/torrent/create.go
+++ commands/torrent/create.go
@@ -15,14 +15,13 @@
 package torrent
 
 import (
-	"fmt"
-	//  "bytes"
-	//	"os"
-	//	"path/filepath"
-	//	"reflect"
+	"io"
 	"log"
+	"os"
+	"time"
 
 	"github.com/urfave/cli/v2"
+	"github.com/xgfone/bt/bencode"
 	"github.com/xgfone/bt/metainfo"
 )
 
@@ -33,57 +32,143 @@ var createCmd = &cli.Command{
 	Usage:     "generate a .torrent from a directory.",
 	ArgsUsage: "TBD",
 	Flags: []cli.Flag{
-		&cli.StringSliceFlag{
-			Name:    "announce",
-			Aliases: []string{"a"},
-			Value:   cli.NewStringSlice("udp://tracker.openbittorrent.com:80/announce"),
-			Usage:   "list of announce URLs to use",
+		&cli.BoolFlag{
+			Name:    "private",
+			Aliases: []string{"p"},
+			Usage:   "(placeholder) set the private flag (Currently unsupported)",
+		},
+		&cli.BoolFlag{
+			Name:    "no-date",
+			Aliases: []string{"d"},
+			Usage:   "leave the date field unset",
 		},
 		&cli.StringSliceFlag{
 			Name:    "webseed",
 			Aliases: []string{"w"},
 			Usage:   "list of possible web seed URLs to use",
 		},
+		&cli.StringSliceFlag{
+			Name:    "announce",
+			Aliases: []string{"a"},
+			Value:   cli.NewStringSlice("udp://tracker.openbittorrent.com:80/announce"),
+			Usage:   "list of announce URLs to use",
+		},
+		&cli.StringFlag{
+			Name:    "comment",
+			Aliases: []string{"c"},
+			Value:   "",
+			Usage:   "add a comment to the torrent file",
+		},
 		&cli.StringFlag{
 			Name:    "output",
 			Aliases: []string{"o"},
-			Value:   "tools.torrent",
-			Usage:   "path to output the .torrent file",
+			Value:   "",
+			Usage:   "path to output the .torrent file, default is the file or directory name.torent",
+		},
+		&cli.StringFlag{
+			Name:    "name",
+			Aliases: []string{"n"},
+			Value:   "",
+			Usage:   "path to output the .torrent file, default is $name.torrent",
 		},
 		&cli.IntFlag{
 			Name:    "length",
 			Aliases: []string{"l"},
 			Value:   256,
-			Usage:   "path to output the .torrent file",
+			Usage:   "piece length to use in kilobytes, default is 256. mktorrent syntax(powers of 2, 15-32) are also supported.",
 		},
 	},
 	Action: func(ctx *cli.Context) error {
-		return CreateTorrent(ctx)
-		return nil
+		piece_len, name, path, output, comment, announces, webseeds, nodate, private := torrentArgs(ctx)
+		return CreateTorrent(piece_len, name, path, output, comment, announces, webseeds, nodate, private)
 	},
 }
 
-func CreateTorrent(ctx *cli.Context) error {
+func torrentArgs(ctx *cli.Context) (pl int64, n string, p string, o string, c string, an []string, ws []string, nd bool, pr bool) {
 	dirs := ctx.Args().Slice()
 	if len(dirs) > 1 {
-		return fmt.Errorf("Input invalid, please use only one file or directory at ta time.")
+		log.Println("Input invalid, please use only one file or directory at a time.")
+		os.Exit(1)
 	}
-	info, err := metainfo.NewInfoFromFilePath(dirs[0], int64(ctx.Int("length")))
+	p = dirs[0]
+
+	n = ctx.String("name")
+	if n == "" {
+		n = p
+	}
+
+	o = ctx.String("output")
+	if o == "" {
+		o = n + ".torrent"
+	}
+	if ctx.Int("length") < 64 {
+		pl = int64(ctx.Int("length") ^ 2)
+	} else {
+		pl = int64(ctx.Int("length") * 1000)
+	}
+	an = ctx.StringSlice("announce")
+	ws = ctx.StringSlice("webseed")
+	pr = ctx.Bool("private")
+	nd = ctx.Bool("no-date")
+	c = ctx.String("")
+
+	return
+}
+
+func CreateTorrent(piece_len int64, name string, path string, output string, comment string, announces []string, webseeds []string, nodate bool, priv bool) error {
+
+	info, err := metainfo.NewInfoFromFilePath(path, piece_len)
 	if err != nil {
 		return err
 	}
-	log.Println(info.CountPieces(), "pieces")
-	var pieces []byte
-	for n := 0; n < info.CountPieces(); n++ {
-		pieces = append(pieces, info.Piece(n).Hash().Bytes()...)
-	}
-	meta := &metainfo.MetaInfo{
-		InfoBytes: pieces,
-	}
-	infoc, err := meta.Info()
+
+	var mi metainfo.MetaInfo
+	mi.InfoBytes, err = bencode.EncodeBytes(info)
 	if err != nil {
 		return err
 	}
-	log.Println("Info generated", infoc.Name)
-	return nil
+
+	// Set the announce information.
+	switch len(announces) {
+	case 0:
+	case 1:
+		mi.Announce = announces[0]
+	default:
+		mi.AnnounceList = metainfo.AnnounceList{announces}
+	}
+
+	switch len(webseeds) {
+	case 0:
+	default:
+		mi.URLList = metainfo.URLList{}
+		for _, seed := range webseeds {
+			mi.URLList = append(mi.URLList, seed)
+		}
+	}
+
+	if !nodate {
+		mi.CreationDate = time.Now().Unix()
+	}
+
+	//	if private {
+	//	  mi.Private = private
+	//	}
+
+	if comment != "" {
+		mi.Comment = comment
+	}
+
+	/// TODO: set the webseed information like above.
+	/// TODO: set other fields of MetaInfo.
+
+	var out io.WriteCloser = os.Stdout
+	if o := output; o != "" {
+		out, err = os.OpenFile(o, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+		if err != nil {
+			return err
+		}
+		defer out.Close()
+	}
+
+	return mi.Write(out)
 }
